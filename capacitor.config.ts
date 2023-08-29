b56d4f4b765663c27ef990a80f4d7167aa3d6b27
BmartinezS <bastian.ig.mar2@gmail.com>
2023-08-28 23:36:35 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'edificios',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
